1055b2684049d69d5d1345409d6550382ff4eb32
unknown <gercop@users.noreply.github.com>
2022-07-10 10:59:49 -0300

    V1.2

--- src/codificacao_testes/T001_hello_world.java
+++ src/codificacao_testes/T001_hello_world.java
@@ -18,7 +18,7 @@ public class T001_hello_world {
 
 	public static void main(String[] args) {
 		
-		System.out.println( "Hello World!!!" );
+		System.out.println( "10/07/2022: Hello World!!!" );
 		
 		//Outra forma mais arrojada de usar o interface
 		Interface1.print(); 

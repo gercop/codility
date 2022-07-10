d3a8db504390906a8262dae724e78804a74c06e8
unknown <ricardogermanos@alumni.usp.br>
2022-07-10 10:33:42 -0300

    V1.2

--- src/codificacao_testes/T002_binario2decimal.java
+++ src/codificacao_testes/T002_binario2decimal.java
@@ -94,7 +94,7 @@ public class T002_binario2decimal {
 	public static void main(String[] args) {
 		
 		/* Rodar através do terminal: codility > src > condificacao_testes > Txx_xxx > show in local terminal
-		 * Linha de comando de exemplo: java T002_binario2decimal.java "11" "0001101" 
+		 * Linha de comando de exemplo: java src\codificacao_testes\T002_binario2decimal.java "11" "0001101" 
 		 * */
 		if (args.length == 0) {
 			args = new String[5];

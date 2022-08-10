f90b9b9914012a4040fd68e613507aa0420f0666
gercop <{ID}+{gercop}@users.noreply.github.com>
2022-08-10 07:09:00 -0300

    V1.4

--- src/codificacao_topico_10_classes/static_class_V1.java
+++ src/codificacao_topico_10_classes/static_class_V1.java
@@ -9,8 +9,7 @@ class TaxCalculator2002 {
 
 public class static_class_V1 {
 	
-	static class TaxCalculator2010 {
-		
+	static class TaxCalculator2010 {		
 		static double calculate (double total) {
 			return total * .08;
 		}		
@@ -23,8 +22,7 @@ public class static_class_V1 {
 		}		
 	}
 
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) {		
 		System.out.println( TaxCalculator2002.calculate(50) );
 		System.out.println( TaxCalculator2010.calculate(50) );
 		System.out.println( TaxCalculator2022.calculate(50) );
